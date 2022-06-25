177ffe74118389246759ed5f4acfe2654ed44b10
jksan <jksan.jp@gmail.com>
2022-06-25 23:36:24 +0900

    router

---
+++ sake/lib/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(title: const Text('Home'), backgroundColor: Colors.orange),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text("Hello"),
+        Text("Hello"),
+        Text("Hello"),
+        Text("Hello"),
+        Text("Hello"),
+        Text("Hello")
+      ])));
+}
