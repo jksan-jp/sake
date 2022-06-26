5376b91714eadef9017b1bdb70c0a5d1c190dff4
jksan <jksan.jp@gmail.com>
2022-06-26 12:28:07 +0900

    dialog

--- sake/lib/ui/post_screen.dart
+++ sake/lib/ui/post_screen.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:go_router/go_router.dart';
 
 class PostScreen extends StatelessWidget {
   const PostScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Post'),
-          backgroundColor: Colors.lime,
+      appBar: AppBar(
+        title: const Text('Post'),
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () => showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                    title: const Text('AlertDialog Title'),
+                    content: const Text('AlertDialog description'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () => context.go('/'),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  )),
         ),
-      );
+        backgroundColor: Colors.lime,
+      ),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        RatingBar.builder(
+          initialRating: 3,
+          minRating: 1,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+          itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+          itemBuilder: (context, _) => const Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (rating) {
+            // ignore: avoid_print
+            print(rating);
+          },
+        ),
+      ])));
 }
